0bb904f980d6a15d35ca88afa8a0b372fd1e1c52
kriptex <kriptexm@gmail.com>
2020-04-06 06:48:41 +0300

    improve vpn.sh

--- vpn.sh
+++ vpn.sh
@@ -4,8 +4,12 @@ STATUS="$(systemctl is-active openvpn@server.service)"
 if [ "${STATUS}" = "active" ]; then
     sudo systemctl stop openvpn@server.service	
     echo "Stop openvpn@server.service ..."
+    sleep 3 
+    curl ipinfo.io/ip
 else 
     sudo systemctl start openvpn@server.service
-    echo "Start openvpn@server.service ..."  
+    echo "Start openvpn@server.service ..."
+    sleep 3
+    curl ipinfo.io/ip  
     exit 1  
 fi
