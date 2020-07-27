e4b621c89c6a7d328496777714379ec9710143c7
root <kriptexm@gmail.com>
2020-07-27 21:38:20 +0300

    added dell_backup.sh

---
+++ dell_backup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -x
+notify-send "Start backup...       " --icon=dialog-information
+
+rpm -qa | sort | sed -e 's/\([^.]*\).*/\1/' -e 's/\(.*\)-.*/\1/' > /home/kriptex/Documents/list_installed_package.txt
+
+set -o nounset -o errexit
+cd $(dirname "$0")
+date=$(date --iso-8601=seconds)
+test -L latest || ln -s "$date" latest
+rsync --delete-excluded --prune-empty-dirs --archive -F --link-dest=../latest "$@" "./$date"
+rm latest
+ln -s "$date" latest
+notify-send "Backup finished!!!    " --icon=dialog-information
