10a3a37c856bf3940a3847145bffc7cd3e5c453b
db <kriptexm@gmail.com>
2024-09-09 19:19:23 +0100

    Edded new scripts

--- lens_update.sh
+++ lens_update.sh
@@ -7,7 +7,7 @@ fi
 
 LENS_URL="https://api.k8slens.dev/binaries/Lens-2024.8.291605-latest.x86_64.AppImage"
 
-echo "Скачиваем Lens с URL: $LENS_URL"
+echo "Lens download URL: $LENS_URL"
 wget -O /tmp/lens.AppImage "$LENS_URL"
 
 chmod +x /tmp/lens.AppImage
