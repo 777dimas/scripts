911f1caeb77df99a1a5579b1e4d3d1dad270c48b
db <kriptexm@gmail.com>
2024-09-10 14:07:53 +0100

    Edded git/checkout.py

---
+++ git/checkout.py
@@ -0,0 +1,30 @@
+import sys
+
+from myutils import GitHelper
+
+class GitCheckout(GitHelper):
+
+    def run(self, checkout_type, branch_name):
+        if checkout_type == 'n':
+            self.repo.git.checkout('HEAD', b=branch_name)
+            self.repo.git.push('--set-upstream', self.remote, branch_name)
+        elif checkout_type == 'b':
+            self.remote.fetch()
+            self.repo.create_head(branch_name, self.remote.refs[branch_name])
+            self.repo.heads[branch_name].set_tracking_branch(
+                self.remote.refs[branch_name])
+            self.repo.heads[branch_name].checkout()
+        elif checkout_type == 'm':
+            self.repo.git.checkout(self.master)
+            self.repo.git.pull()
+
+def main():
+    helper = GitCheckout()
+    if len(sys.argv) != 3:
+        helper.logger.error("Must pass branch name!")
+        sys.exit(1)
+    helper.run(sys.argv[1], sys.argv[2])
+
+
+if __name__ == "__main__":
+    main()
